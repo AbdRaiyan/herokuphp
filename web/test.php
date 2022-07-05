166f4f7258ae446e24374ac3ae44740aa201c125
AbdRaiyan <20022280@myrp.edu.sg>
2022-07-05 10:49:46 +0800

    update test.php

--- web/test.php
+++ web/test.php
@@ -1,8 +1,13 @@
+<!--fix: DOCTYPE added -->
 <!DOCTYPE html>
+<!-- fix: lang attribute added -->
 <html lang="en">
 <head>
+    <!-- fix: Lack of meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+    <!-- fix: lack of title -->
     <title>Woah a heroku app</title>
   Hello World!
 </body>
